dfd9232cfd3f8018f59e7c58ef34cf938e507e0d
karima-bou10 <karima.bouachra1010@gmail.com>
2024-03-28 10:56:17 +0000

    Add DriverMapper

---
+++ Server/src/main/java/org/example/server/Affectation/Mapper/DriverMapper.java
@@ -0,0 +1,27 @@
+package org.example.server.Affectation.Mapper;
+
+import org.example.server.Affectation.Model.Driver;
+import org.example.server.Affectation.Model.Trip;
+import org.example.server.Affectation.dto.DriverDTO;
+import org.example.server.Affectation.dto.TripDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DriverMapper {
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public DriverMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public DriverDTO toDriverDTO(Driver driver) {
+        return modelMapper.map(driver, DriverDTO.class);
+    }
+
+    public Driver toDriver(DriverDTO driverDTO) {
+        return modelMapper.map(driverDTO, Driver.class);
+    }
+}
